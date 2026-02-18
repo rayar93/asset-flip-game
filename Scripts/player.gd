003bd719acb88773ab64930a92071b07af23a719
rayar93 <156842273+rayar93@users.noreply.github.com>
2026-02-18 15:09:00 -0500

    Added player animations.

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -7,11 +7,14 @@ extends CharacterBody2D
 @export var attack_duration = 0.1 # seconds
 
 @onready var attack_area: Area2D = $AttackArea # rename AttackArea node
+@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 
 var facing = 1 # 1 is right, -1 is left
+var is_attacking = false
 
 func _ready():
 	attack_area.monitoring = false
+	anim.play("idle")
 
 # _physics_process() is called by the engine every tick
 # we use this instead of _process so that physics is independent of framerate
@@ -32,16 +35,34 @@ func _physics_process(delta):
 	else:
 		velocity.x = 0
 		
+	anim.flip_h = (facing == -1) # flip sprite when player is facing left
 	attack_area.position.x = abs(attack_area.position.x) * facing
 	
 	# Handle attacking
 	if Input.is_action_just_pressed("attack"):
 		do_attack()
 	
+	# Pick an animation
+	if not is_attacking:
+		if not is_on_floor():
+			if velocity.y < 0:
+				play_anim("jump")
+			else:
+				play_anim("fall")
+		elif abs(velocity.x) > 0:
+			play_anim("run")
+		else:
+			play_anim("idle")
+	
 	# move_and_slide() takes our velocity and handles collision
 	move_and_slide()
 	
+func play_anim(anim_name: String):
+	if anim.animation != anim_name:
+		anim.play(anim_name)
+	
 func do_attack():
+	is_attacking = true
 	attack_area.monitoring = true
 	await get_tree().physics_frame
 	
@@ -51,29 +72,5 @@ func do_attack():
 			
 	await get_tree().create_timer(attack_duration).timeout
 	attack_area.monitoring = false
-
-""" Below is the default script for CharacterBody2D, for reference.
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
-"""
+	
+	is_attacking = false
