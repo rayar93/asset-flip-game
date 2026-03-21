f4001b2b901d34f433d07ea8779ec0e0603d9e83
rayar93 <156842273+rayar93@users.noreply.github.com>
2026-03-21 13:03:13 -0400

    Health UI fix + change

--- UI/health_ui.gd
+++ UI/health_ui.gd
@@ -1,11 +1,27 @@
 extends CanvasLayer
 
 @onready var pips = $HBoxContainer.get_children()
+var player = null
 
 func _ready() -> void:
-	var player = get_tree().get_first_node_in_group("player")
-	player.health_changed.connect(_on_health_changed)
+	get_tree().node_added.connect(_on_node_added)
+	await _find_and_connect_player()
+	
+func _on_node_added(node: Node):
+	if node.is_in_group("player"):
+		await _find_and_connect_player()
+
+func _find_and_connect_player():
+	await get_tree().process_frame
+	player = get_tree().get_first_node_in_group("player")
+	if not is_instance_valid(player): return
+	_update_pips(player.current_health)
+	if not player.health_changed.is_connected(_on_health_changed):
+		player.health_changed.connect(_on_health_changed)
 	
 func _on_health_changed(new_health):
+	_update_pips(new_health)
+	
+func _update_pips(current_health):
 	for i in pips.size():
-		pips[i].modulate = Color.WHITE if i < new_health else Color(0.2, 0.2, 0.2)
+		pips[i].modulate = Color.WHITE if i < current_health else Color(0.2, 0.2, 0.2, 0.5)
