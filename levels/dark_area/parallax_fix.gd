09498d85effc8e8bc5eaa02f5cad0b4f131ce130
rayar93 <156842273+rayar93@users.noreply.github.com>
2026-03-22 03:20:26 -0400

    level 2 changes

--- levels/dark_area/parallax_fix.gd
+++ levels/dark_area/parallax_fix.gd
@@ -1,5 +1,14 @@
 extends Parallax2D
 
+var base_offset_y = 0.0
+
+func _ready():
+	await get_tree().process_frame
+	var cam = get_viewport().get_camera_2d()
+	if cam:
+		base_offset_y = cam.offset.y
+
 func _process(delta: float) -> void:
 	var cam = get_viewport().get_camera_2d()
-	scroll_offset.y = -cam.global_position.y
+	if not cam: return
+	scroll_offset.y = -(cam.global_position.y + base_offset_y)
