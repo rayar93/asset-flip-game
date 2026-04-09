5b2892ea2db992961bd42e96776735200ec02a46
rayar93 <156842273+rayar93@users.noreply.github.com>
2026-04-09 02:43:15 -0400

    Updated sprints, removed prints

--- autoloads/VFX.gd
+++ autoloads/VFX.gd
@@ -10,13 +10,10 @@ var camera: Camera2D
 func _ready():
 	await get_tree().process_frame
 	camera = get_tree().get_first_node_in_group("camera")
-	print("VFX camera: ", camera)
 	if is_instance_valid(camera):
 		base_offset = camera.offset
-		print("camera: ", base_offset)
 
 func screenshake(amount: float = 0.5, m_offset = 12.0):
-	print("Screenshake: ", camera)
 	trauma = min(trauma + amount, 1.0)
 	max_offset = m_offset
 	
