46d53f4845c91ba64a548fe932721f623f8924c3
rayar93 <156842273+rayar93@users.noreply.github.com>
2026-04-16 21:01:33 -0400

    fixed dash VFX

--- Actors/Player/player.gd
+++ Actors/Player/player.gd
@@ -36,7 +36,7 @@ var state := State.AIR
 var facing = 1
 var current_health = max_health
 
-#const SmokeEffect = preload("C:/Users/alanr/OneDrive - Appalachian State University/Documents/GitHub/Capstone-Project/actors/player/dash.tscn")
+const SmokeEffect = preload("res://actors/player/dash.tscn")
 
 signal health_changed(new_health: int)
 
@@ -100,7 +100,7 @@ func set_state(new_state: State):
 			dash_time_left = dash_duration
 			velocity = Vector2(facing * dash_speed, 0)
 			can_dash = false
-			#_spawn_dash_smoke()
+			_spawn_dash_smoke()
 		State.ATTACK:
 			_execute_attack_startup()
 		State.HURT:
@@ -224,12 +224,12 @@ func _return_to_base_state():
 	slash_vfx.hide()
 	set_state(State.GROUNDED if is_on_floor() else State.AIR)
 
-#func _spawn_dash_smoke():
-	#var smoke = SmokeEffect.instantiate()
-	#get_parent().add_child(smoke)
-	#smoke.global_position = global_position
-	#smoke.scale.x = facing
-	#smoke.play()
+func _spawn_dash_smoke():
+	var smoke = SmokeEffect.instantiate()
+	get_parent().add_child(smoke)
+	smoke.global_position = global_position
+	smoke.scale.x = facing
+	smoke.play()
 
 func die():
 	set_state(State.DEAD)
