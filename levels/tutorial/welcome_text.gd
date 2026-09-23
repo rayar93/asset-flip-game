4764417e53333e43916152275e8e172e7b430d0b
rayar93 <156842273+rayar93@users.noreply.github.com>
2026-09-23 02:40:54 -0400

    Cleaned up a few GB of unused asset files

--- levels/tutorial/welcome_text.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-@onready var anim_player = $AnimationPlayer
-
-func _ready():
-	connect("body_entered", _on_body_entered)
-
-func _on_body_entered(body):
-	anim_player.play("Show")
